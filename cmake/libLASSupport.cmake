384f1bb023c8a6fd365df746c6b6d53dfc86ac41
Andy Maloney <asmaloney@gmail.com>
2020-03-30 14:55:01 -0400

    {cmake} Clean up library finding

--- cmake/libLASSupport.cmake
+++ cmake/libLASSupport.cmake
@@ -1,7 +1,6 @@
 
 set( LIBLAS_INCLUDE_DIR "" CACHE PATH "libLAS include directory" )
 set( LIBLAS_LIB_DIR "" CACHE PATH "libLAS library directory" )
-set( LIBLAS_RELEASE_LIBRARY_FILE "" CACHE FILEPATH "libLAS library file (release mode)" )
 
 if ( NOT LIBLAS_INCLUDE_DIR )
 	message( SEND_ERROR "No libLAS include dir specified (LIBLAS_INCLUDE_DIR)" )
@@ -9,14 +8,15 @@ else()
 	include_directories( ${LIBLAS_INCLUDE_DIR} )
 endif()
 
-if ( NOT LIBLAS_RELEASE_LIBRARY_FILE )
-	message( SEND_ERROR "No libLAS lib specified (LIBLAS_RELEASE_LIBRARY_FILE)" )
+if ( NOT LIBLAS_LIB_DIR )
+	message( SEND_ERROR "No libLAS lib directory specified (LIBLAS_LIB_DIR)" )
 endif()
 
 function( target_link_liblas )
-	if ( LIBLAS_RELEASE_LIBRARY_FILE )
-		target_link_libraries( ${PROJECT_NAME} ${LIBLAS_RELEASE_LIBRARY_FILE} )
-		target_link_libraries( ${PROJECT_NAME} ${LIBLAS_LIB_DIR}/liblas_c.a )
-		target_link_libraries( ${PROJECT_NAME} ${LIBLAS_LIB_DIR}/liblaszip.a )
+	if ( LIBLAS_LIB_DIR )
+		target_link_libraries( ${PROJECT_NAME}
+			${LIBLAS_LIB_DIR}/liblas.a
+			${LIBLAS_LIB_DIR}/liblas_c.a
+			${LIBLAS_LIB_DIR}/liblaszip.a )
 	endif()
 endfunction()
